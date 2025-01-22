7764a1748a4224c1b9b1589762dd2635b66ef128
Nat <usolnatalia@gmail.com>
2025-01-22 21:25:07 +1300

    Fix the endpoint for getRecipes

--- recipe/app/src/main/java/helpers/Endpoints.kt
+++ recipe/app/src/main/java/helpers/Endpoints.kt
@@ -1,13 +1,14 @@
 package helpers
 
 import com.example.recipe.helpers.ApiInterface
+import data.Recipe
 
 class Endpoints {
     private var apiInterface: ApiInterface = RetrofitInstance.getInstance().create(ApiInterface::class.java)
 
-    suspend fun getSpecies(page: Number = 1): List<Species>? {
+    suspend fun getRecipes(): List<Recipe>? {
         try {
-            val response = apiInterface.getSpecies(page)
+            val response = apiInterface.getRecipes()
             if (response.isSuccessful) {
                 val data = response.body()
                 return data
@@ -17,22 +18,6 @@ class Endpoints {
             }
         } catch (e: Exception) {
             println("Exception error getSpecies: ${e.message}. ${e.printStackTrace()}")
-            return null
-        }
-    }
-
-    suspend fun searchSpecies(searchQuery: String = "", page: Number = 1): List<Species>? {
-        try {
-            val response = apiInterface.searchSpecies(searchQuery, page)
-            if (response.isSuccessful) {
-                val data = response.body()
-                return data
-            } else {
-                println("Response error: $response")
-                return null
-            }
-        } catch (e: Exception) {
-            println("Exception error: ${e.message}")
             return null
         }
     }
