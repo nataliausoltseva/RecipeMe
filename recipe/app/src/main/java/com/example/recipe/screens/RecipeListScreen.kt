b97e3b85986bf2be7963aaded4bf3356f1556965
Nat <usolnatalia@gmail.com>
2025-08-15 21:11:28 +1200

    Add clear button on the search input

--- recipe/app/src/main/java/com/example/recipe/screens/RecipeListScreen.kt
+++ recipe/app/src/main/java/com/example/recipe/screens/RecipeListScreen.kt
@@ -54,6 +54,7 @@ import androidx.compose.material3.DropdownMenuItem
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.FloatingActionButton
 import androidx.compose.material3.Icon
+import androidx.compose.material3.IconButton
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.PlainTooltip
 import androidx.compose.material3.Surface
@@ -963,7 +964,17 @@ fun SearchInput(
         shape = RoundedCornerShape(50),
         modifier = Modifier
             .fillMaxWidth(0.75F),
-        singleLine = true
+        singleLine = true,
+        trailingIcon = {
+            if (search.isNotEmpty()) {
+                IconButton(onClick = { onValueChange("") }) {
+                    Icon(
+                        imageVector = Icons.Default.Clear,
+                        contentDescription = "Clear Search"
+                    )
+                }
+            }
+        },
     )
 }
 
